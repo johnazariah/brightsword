e448dc322ea9e1827014db583946c7bf18032d50
John Azariah <john.azariah@gmail.com>
2025-10-02 16:32:19 +1000

    chore(cleanup): simplify Disposable<T> to conventional class with ctor

--- BrightSword.SwissKnife/Disposable.cs
+++ BrightSword.SwissKnife/Disposable.cs
@@ -2,11 +2,17 @@ using System;
 
 namespace BrightSword.SwissKnife
 {
-    public class Disposable<T>(T instance, Action<T> dispose) : IDisposable
+    public sealed class Disposable<T> : IDisposable
     {
-        private readonly Action<T> _dispose = dispose;
+        private readonly Action<T> _dispose;
 
-        public T Instance { get; } = instance;
+        public T Instance { get; }
+
+        public Disposable(T instance, Action<T> dispose)
+        {
+            Instance = instance;
+            _dispose = dispose ?? (_ => { });
+        }
 
         public void Dispose()
         {
