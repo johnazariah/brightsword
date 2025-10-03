b5c285c26326f3f0c803edc74dfb3c17fa4ceafa
John Azariah <john.azariah@gmail.com>
2025-10-04 08:41:50 +1000

    removed old build approach

--- BrightSword.Crucible/ExceptionHelper.cs
+++ BrightSword.Crucible/ExceptionHelper.cs
@@ -1,42 +1,27 @@
-// Decompiled with JetBrains decompiler
-// Type: BrightSword.Crucible.ExceptionHelper
-// Assembly: BrightSword.Crucible, Version=1.0.16.0, Culture=neutral, PublicKeyToken=null
-// MVID: 719B4D8D-7577-4C5C-8115-083F94C056BF
-// Assembly location: C:\Users\johnaz\AppData\Local\Temp\Depejin\bc05fa93a2\lib\net40\BrightSword.Crucible.dll
-
 using System;
 
-#nullable disable
-namespace BrightSword.Crucible;
-
-public static class ExceptionHelper
+namespace BrightSword.Crucible
 {
-    public static TException ExpectException<TException>(
-      this Action action,
-      string expectedExceptionMessage = null)
-      where TException : Exception
+    public static class ExceptionHelper
     {
-        try
+        public static TException ExpectException<TException>(
+          this Action action,
+          string expectedExceptionMessage = null)
+          where TException : Exception
         {
-            action();
-            throw new InvalidOperationException($"An exception of type '{typeof(TException).Name}' was expected but not thrown");
-        }
-        catch (Exception ex)
-        {
-            if (ex is TException exception)
+            try
             {
-                if (string.IsNullOrWhiteSpace(expectedExceptionMessage) || exception.Message.Equals(expectedExceptionMessage, StringComparison.OrdinalIgnoreCase))
-                {
-                    return exception;
-                }
-
-                throw new InvalidOperationException($"An exception of type '{typeof(TException).Name}' with message '{expectedExceptionMessage}' was expected. The exception was thrown but the exception message was '{exception.Message}'.");
+                action();
+                throw new InvalidOperationException($"An exception of type '{typeof(TException).Name}' was expected but not thrown");
             }
-
-            throw new InvalidOperationException($"An exception of type '{typeof(TException).Name}' was expected, but an exception of type '{ex.GetType().Name}', with message '{ex.Message}' was thrown instead.");
+            catch (Exception ex)
+            {
+                return ex is TException exception
+                    ? string.IsNullOrWhiteSpace(expectedExceptionMessage) || exception.Message.Equals(expectedExceptionMessage, StringComparison.OrdinalIgnoreCase)
+                        ? exception
+                        : throw new InvalidOperationException($"An exception of type '{typeof(TException).Name}' with message '{expectedExceptionMessage}' was expected. The exception was thrown but the exception message was '{exception.Message}'.")
+                    : throw new InvalidOperationException($"An exception of type '{typeof(TException).Name}' was expected, but an exception of type '{ex.GetType().Name}', with message '{ex.Message}' was thrown instead.");
+            }
         }
-        // unreachable but required for compile-time return
-        // ReSharper disable once HeuristicUnreachableCode
-        // return default;
     }
 }
